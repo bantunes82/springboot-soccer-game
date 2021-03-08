029a0ba3e09e2b9283bd60a23f337a79c0fade25
bruno.romao <bruno.romao@edreamsodigeo.com>
2021-03-08 08:21:26 +0100

    Creating the project structure with the minimal dependencies

---
+++ rest-team/src/main/java/springboot/soccer/game/team/RestTeamApplication.java
@@ -0,0 +1,13 @@
+package springboot.soccer.game.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTeamApplication.class, args);
+	}
+
+}
---
+++ rest-team/src/test/java/springboot/soccer/game/team/RestTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.soccer.game.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
