68a8f7a9cd527b4d382b4eb9d87e0bdb08742f73
Bruno, Romao Antunes <bantunes82@gmail.com>
2021-04-21 21:28:53 +0200

    Configuring prometheus, grafana and creating importing a custom SpringBoot dashboard

--- rest-team/src/main/java/springboot/soccer/game/team/resource/TeamResource.java
+++ rest-team/src/main/java/springboot/soccer/game/team/resource/TeamResource.java
@@ -1,6 +1,5 @@
 package springboot.soccer.game.team.resource;
 
-import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.headers.Header;
@@ -62,7 +61,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "200", description = "When there is at least one soccer team available", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TeamDTO.class, required = true))),
             @ApiResponse(responseCode = "404", description = "When there is no soccer team available", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)))
     })
-    @Timed(value = "timeFindRandomTeam", description = "Times how long it takes to invoke the findRandomTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+   // @Timed(value = "timeFindRandomTeam", description = "Times how long it takes to invoke the findRandomTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @GetMapping(path = "/random")
     public ResponseEntity<TeamDTO> findRandomTeam() throws EntityNotFoundException {
         TeamDO teamRandom = teamService.findRandom();
@@ -72,7 +71,7 @@ public class TeamResource {
 
     @Operation(summary = "Returns a list of soccer teams that has the specified name")
     @ApiResponses(@ApiResponse(responseCode = "200", description = "Returns a list of soccer teams for the specified name", content = @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = TeamDTO.class, required = true)))))
-    @Timed(value = "timeFindTeamByName", description = "Times how long it takes to invoke the findTeamByName method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeFindTeamByName", description = "Times how long it takes to invoke the findTeamByName method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @GetMapping(path = "/name/{name}")
     public ResponseEntity<List<TeamDTO>> findTeamByName(@Parameter(description = "soccer team name", required = true) @PathVariable("name") String name) {
         List<TeamDO> teams = teamService.findByName(name);
@@ -85,7 +84,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "200", description = "Returns a list the soccer teams for the specified Country Code", content = @Content(mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = TeamDTO.class, required = true)))),
             @ApiResponse(responseCode = "400", description = "When the countryCode is invalid", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)))
     })
-    @Timed(value = "timeFindTeamByCountryCode", description = "Times how long it takes to invoke the findTeamByCountryCode method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeFindTeamByCountryCode", description = "Times how long it takes to invoke the findTeamByCountryCode method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @GetMapping(path = "/country/{countryCode}")
     public ResponseEntity<List<TeamDTO>> findTeamByCountryCode(@Parameter(description = "country code", required = true) @PathVariable("countryCode") @CountryCode String countryCode,
                                                                @Parameter(description = "page index") @RequestParam(value = "pageIndex", defaultValue = "0") int pageIndex,
@@ -100,7 +99,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "201", description = "The URI of the created soccer team", headers = {@Header(name = LOCATION, description = "URI location of the created soccer team", schema = @Schema(implementation = URI.class))}),
             @ApiResponse(responseCode = "400", description = "When the content of the TeamDTO is invalid", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)))
     })
-    @Timed(value = "timeCreateTeam", description = "Times how long it takes to invoke the createTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeCreateTeam", description = "Times how long it takes to invoke the createTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createTeam(@io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TeamDTO.class)), description = "soccer team to be created") @Valid @RequestBody TeamDTO teamDTO) {
         TeamDO teamDO = teamMapper.toTeamDO(teamDTO);
@@ -121,7 +120,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "404", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)), description = "When there is no soccer team available for the specified id"),
             @ApiResponse(responseCode = "400", description = "When the content of the TeamDTO is invalid", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)))
     })
-    @Timed(value = "timeUpdateTeam", description = "Times how long it takes to invoke the updateTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeUpdateTeam", description = "Times how long it takes to invoke the updateTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @PutMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<TeamDTO> updateTeam(@Parameter(required = true, description = "soccer team id") @PathVariable("id") Long teamId,
                                               @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true, content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TeamDTO.class)), description = "soccer team to be updated") @Valid @RequestBody TeamDTO teamDTO) throws EntityNotFoundException {
@@ -137,7 +136,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "404", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)), description = "When there is no soccer team available for the specified id"),
             @ApiResponse(responseCode = "400", description = "When the team level is invalid", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)))
     })
-    @Timed(value = "timeUpdateTeamLevel", description = "Times how long it takes to invoke the updateTeamLevel method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeUpdateTeamLevel", description = "Times how long it takes to invoke the updateTeamLevel method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @PatchMapping(path = "/{id}/level/{value}")
     public ResponseEntity<TeamDTO> updateTeamLevel(@Parameter(required = true, description = "soccer team id") @PathVariable("id") Long teamId,
                                                    @Parameter(required = true, description = "soccer team level") @Range(min = 1.0, max = 10.0) @PathVariable("value") Double level) throws EntityNotFoundException {
@@ -151,7 +150,7 @@ public class TeamResource {
             @ApiResponse(responseCode = "204", description = "Soccer Team deleted"),
             @ApiResponse(responseCode = "404", content = @Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorDTO.class, required = true)), description = "When there is no soccer team available for the specified id"),
     })
-    @Timed(value = "timeDeleteTeam", description = "Times how long it takes to invoke the deleteTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
+    //@Timed(value = "timeDeleteTeam", description = "Times how long it takes to invoke the deleteTeam method", histogram = true, percentiles = {0.5,0.75,0.95,0.98,0.99,0.999})
     @DeleteMapping(path = "{id}")
     public ResponseEntity<Void> deleteTeam(@Parameter(required = true, description = "soccer team id") @PathVariable("id") Long teamId) throws EntityNotFoundException {
         teamService.delete(teamId);
