2c7e4e66c516dfe90197bb809e54e9b21810538a
Bruno, Romao Antunes <bantunes82@gmail.com>
2021-05-03 08:22:32 +0200

    Configuring Readiness probe to use Keycloak and fix the integrationTest

--- rest-team/src/main/java/springboot/soccer/game/team/health/KeycloakHealthIndicator.java
+++ rest-team/src/main/java/springboot/soccer/game/team/health/KeycloakHealthIndicator.java
@@ -1,13 +1,46 @@
 package springboot.soccer.game.team.health;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Component
 public class KeycloakHealthIndicator implements HealthIndicator {
+
+    @Value("${keycloak.auth-server-url}/realms/${keycloak.realm}")
+    private String oidcAuthServerUrl;
+
+    private RestTemplate restTemplate = new RestTemplate();
+
     @Override
     public Health health() {
-        return Health.up().withDetail("Keycloak connections health check",200).build();
+        if (checkHealthOfOidcAuthServer()) {
+            return Health.up().withDetail("Keycloak connections health check", "Success").build();
+        } else {
+            return Health.down().withDetail("Keycloak connections health check", "Fail").build();
+        }
+    }
+
+    private boolean checkHealthOfOidcAuthServer() {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            return restTemplate
+                    .exchange(oidcAuthServerUrl, HttpMethod.GET, entity, Void.class)
+                    .getStatusCode() == HttpStatus.OK;
+        } catch (RuntimeException ex) {
+            return false;
+        }
     }
 }
--- rest-team/src/test/java/springboot/soccer/game/team/health/HealthIntegrationTest.java
+++ rest-team/src/test/java/springboot/soccer/game/team/health/HealthIntegrationTest.java
@@ -8,12 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.Duration;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,20 +30,31 @@ public class HealthIntegrationTest {
 
     private static String HEALTH_PATH = "/actuator/health";
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @Container
-    private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>("postgres:13.2")
+    private static PostgreSQLContainer DATABASE = new PostgreSQLContainer<>("postgres:13.2")
             .withDatabaseName("teams_database")
             .withUsername("team")
             .withPassword("team");
+    @Container
+    private static GenericContainer IDENTITY_ACCESS_MANAGEMENT = new GenericContainer("quay.io/keycloak/keycloak:12.0.4")
+            .withCommand("-b 0.0.0.0 -Djboss.http.port=8082 -Dkeycloak.profile.feature.upload_scripts=enabled -Dkeycloak.migration.action=import " +
+                    "-Dkeycloak.migration.provider=dir -Dkeycloak.migration.dir=/tmp/keycloak/realms -Dkeycloak.migration.strategy=OVERWRITE_EXISTING")
+            .withClasspathResourceMapping("./keycloak/realms/", "/tmp/keycloak/realms/", BindMode.READ_ONLY)
+            .withStartupTimeout(Duration.ofSeconds(120))
+            .withExposedPorts(8082)
+            .withEnv("DB_VENDOR", "h2")
+            .waitingFor(Wait.forHttp("/auth"));
+    @Autowired
+    private MockMvc mockMvc;
 
     @DynamicPropertySource
     static void registryProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.url", DATABASE::getJdbcUrl);
+        registry.add("spring.datasource.password", DATABASE::getPassword);
+        registry.add("spring.datasource.username", DATABASE::getUsername);
+
+        String authServerUrl = String.format("http://%s:%d/auth", IDENTITY_ACCESS_MANAGEMENT.getHost(), IDENTITY_ACCESS_MANAGEMENT.getMappedPort(8082));
+        registry.add("keycloak.auth-server-url", () -> authServerUrl);
     }
 
     @Test
@@ -58,7 +74,7 @@ public class HealthIntegrationTest {
         )
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(APPLICATION_JSON))
-                .andExpect(content().json("{\"status\":\"UP\",\"components\":{\"db\":{\"status\":\"UP\",\"details\":{\"database\":\"PostgreSQL\",\"validationQuery\":\"isValid()\"}},\"keycloak\":{\"status\":\"UP\",\"details\":{\"Keycloak connections health check\":200}},\"readinessState\":{\"status\":\"UP\"}}}"));
+                .andExpect(content().json("{\"status\":\"UP\",\"components\":{\"db\":{\"status\":\"UP\",\"details\":{\"database\":\"PostgreSQL\",\"validationQuery\":\"isValid()\"}},\"keycloak\":{\"status\":\"UP\",\"details\":{\"Keycloak connections health check\":Success}},\"readinessState\":{\"status\":\"UP\"}}}"));
 
     }
 
@@ -71,7 +87,7 @@ public class HealthIntegrationTest {
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(content().json("{\"status\":\"UP\"}"))
                 .andExpect(content().json("{\"components\":{\"db\":{\"status\":\"UP\",\"details\":{\"database\":\"PostgreSQL\",\"validationQuery\":\"isValid()\"}}}}"))
-                .andExpect(content().json("{\"components\":{\"keycloak\":{\"status\":\"UP\",\"details\":{\"Keycloak connections health check\":200}}}}"))
+                .andExpect(content().json("{\"components\":{\"keycloak\":{\"status\":\"UP\",\"details\":{\"Keycloak connections health check\":Success}}}}"))
                 .andExpect(content().json("{\"components\":{\"livenessState\":{\"status\":\"UP\"}}}}"))
                 .andExpect(content().json("{\"components\":{\"ping\":{\"status\":\"UP\"}}}}"))
                 .andExpect(content().json("{\"components\":{\"readinessState\":{\"status\":\"UP\"}}}}"))
