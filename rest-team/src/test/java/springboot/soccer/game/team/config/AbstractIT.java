ac287c82924d7dad660dd0ddf28bde11aa1d9257
Bruno, Romao Antunes <bantunes82@gmail.com>
2021-05-05 13:36:51 +0200

    Revert "Refactoring the IntegrationTests"
    This reverts commit 368a6566

--- rest-team/src/test/java/springboot/soccer/game/team/config/AbstractIT.java
+++ rest-team/src/test/java/springboot/soccer/game/team/config/AbstractIT.java
@@ -16,7 +16,7 @@ import java.time.Duration;
 @Testcontainers
 public abstract class AbstractIT {
 
-   @Container
+    @Container
     private static PostgreSQLContainer DATABASE = new PostgreSQLContainer<>("postgres:13.2")
             .withDatabaseName("teams_database")
             .withUsername("team")
@@ -40,14 +40,14 @@ public abstract class AbstractIT {
 
         String authServerUrl = String.format("http://%s:%d/auth", IDENTITY_ACCESS_MANAGEMENT.getHost(), IDENTITY_ACCESS_MANAGEMENT.getMappedPort(8082));
         registry.add("keycloak.auth-server-url", () -> authServerUrl);
-        registry.add("keycloak.realm", () -> KeycloakConfiguration.kEYCLOAK_REALM);
-        registry.add("keycloak.resource", () -> KeycloakConfiguration.kEYCLOAK_RESOURCE);
-        registry.add("keycloak.bearer-only", () -> KeycloakConfiguration.kEYCLOAK_BEARER_ONLY);
-        registry.add("keycloak.security-constraints[0].authRoles[0]", () -> KeycloakConfiguration.kEYCLOAK_AUTH_ROLES);
-        registry.add("keycloak.security-constraints[0].securityCollections[0].patterns[0]", () -> KeycloakConfiguration.kEYCLOAK_PATTERNS);
-        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[0]", () -> KeycloakConfiguration.kEYCLOAK_METHOD0);
-        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[1]", () -> KeycloakConfiguration.kEYCLOAK_METHOD1);
-        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[2]", () -> KeycloakConfiguration.kEYCLOAK_METHOD2);
-        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[3]", () -> KeycloakConfiguration.kEYCLOAK_METHOD3);
+        registry.add("keycloak.realm", () -> "team-realm");
+        registry.add("keycloak.resource", () -> "team-client");
+        registry.add("keycloak.bearer-only", () -> "true");
+        registry.add("keycloak.security-constraints[0].authRoles[0]", () -> "team");
+        registry.add("keycloak.security-constraints[0].securityCollections[0].patterns[0]", () -> "/v1/teams/*");
+        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[0]", () -> "POST");
+        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[1]", () -> "PUT");
+        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[2]", () -> "PATCH");
+        registry.add("keycloak.security-constraints[0].securityCollections[0].methods[3]", () -> "DELETE");
     }
 }
