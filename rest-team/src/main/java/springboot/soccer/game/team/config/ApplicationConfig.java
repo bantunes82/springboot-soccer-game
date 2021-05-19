2011da290684cb6e29e190fac88c5362725ba90e
Bruno, Romao Antunes <bantunes82@gmail.com>
2021-05-19 10:05:41 +0200

    Refactoring to move to the same place all the Configuration related

---
+++ rest-team/src/main/java/springboot/soccer/game/team/config/ApplicationConfig.java
@@ -0,0 +1,64 @@
+package springboot.soccer.game.team.config;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Team Soccer API",
+                description = "This API allows CRUD operations on a soccer team",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Bruno Romao Antunes",
+                        url = "https://github.com/bantunes82",
+                        email = "bantunes82@gmail.com")),
+        externalDocs = @ExternalDocumentation(url = "https://github.com/bantunes82/springboot-soccer-game/tree/main/rest-team")
+)
+@SecuritySchemes(value = {
+        @SecurityScheme(name = "accessToken",
+                type = SecuritySchemeType.HTTP,
+                description = "Access token for the user that belongs to TEAM role",
+                scheme = "Bearer")}
+)
+@Configuration
+public class ApplicationConfig {
+
+    private static final String CLASSPATH_MESSAGES = "classpath:messages";
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(CLASSPATH_MESSAGES);
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
