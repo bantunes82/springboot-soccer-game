73155a8dd8c1d90ffeb08ec248451216deb47957
bantunes82 <bantunes82@gmail.com>
2024-01-19 19:13:45 +0100

    Converting to use Text Block from Java 15

--- rest-team/src/main/java/springboot/soccer/game/team/datatransferobject/ErrorDTO.java
+++ rest-team/src/main/java/springboot/soccer/game/team/datatransferobject/ErrorDTO.java
@@ -9,13 +9,16 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-@Schema(description = "Errors", required = true, example = "{\n" +
-        "  \"timestamp\": \"2021-04-03T14:57:59.670094Z\",\n" +
-        "  \"errors\": {\n" +
-        "    \"error\": \"Could not find team with id: 234\"\n" + ", " +
-        "    \"name\": \"Team name must be between 3 and 50 chars\"\n" +
-        "  }\n" +
-        "}")
+@Schema(description = "Errors", required = true, example =
+        """
+            {
+                "timestamp": "2021-04-03T14:57:59.670094Z",
+                "errors": {
+                    "error": "Could not find team with id: 234",
+                    "name": "Team name must be between 3 and 50 chars"
+                 }
+            }
+        """)
 @Getter
 @NoArgsConstructor
 public class ErrorDTO {
