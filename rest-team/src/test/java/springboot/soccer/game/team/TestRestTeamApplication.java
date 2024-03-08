f543fd63e0cb15b0f7356a36a0fc06466dbdf990
bantunes82 <bantunes82@gmail.com>
2024-03-08 14:58:45 +0100

    Improve configuration with TestContainers

--- rest-team/src/test/java/springboot/soccer/game/team/TestRestTeamApplication.java
+++ rest-team/src/test/java/springboot/soccer/game/team/TestRestTeamApplication.java
@@ -8,12 +8,8 @@ import java.util.Arrays;
 public class TestRestTeamApplication {
 
     public static void main(String[] args) {
-        if(args.length>0){
-            args = Arrays.copyOf(args, args.length + 1);
-            args[args.length-1] = "--spring.profiles.active=test";
-        }else {
-            args = new String[]{"--spring.profiles.active=test"};
-        }
+        args = Arrays.copyOf(args, args.length + 1);
+        args[args.length-1] = "--spring.profiles.active=test";
 
         SpringApplication.from(RestTeamApplication::main)
                 .with(TestContainersConfiguration.class)
