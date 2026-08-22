a8fbf219f8b3013da0cacd84983a73b311a7096d
Bruno Romao <bantunes82@gmail.com>
2026-08-22 16:44:50 +0200

    -- fixing issue to run the application in native mode: jakarta.servlet.ServletException: Handler dispatch failed: org.graalvm.nativeimage.MissingReflectionRegistrationError: Cannot reflectively read or write field 'private final org.springframework.boot.webmvc.actuate.endpoint.web.AbstractWebMvcEndpointHandlerMapping$ServletWebOperation org.springframework.boot.webmvc.actuate.endpoint.web.AbstractWebMvcEndpointHandlerMapping$OperationHandler.operation'. To allow this operation, add the following to the 'reflection' section of 'reachability-metadata.json' and rebuild the native image:

--- rest-team/src/main/java/springboot/soccer/game/team/config/RestTeamRuntimeHints.java
+++ rest-team/src/main/java/springboot/soccer/game/team/config/RestTeamRuntimeHints.java
@@ -1,7 +1,9 @@
 package springboot.soccer.game.team.config;
 
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 
 public class RestTeamRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -9,5 +11,23 @@ public class RestTeamRuntimeHints implements RuntimeHintsRegistrar {
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         // Register resources
         hints.resources().registerPattern("messages*");
+
+        // Register Spring Boot Actuator endpoint handler for reflection
+        // Required for native image compilation to avoid MissingReflectionRegistrationError
+        registerActuatorReflectionHints(hints);
+    }
+
+    private void registerActuatorReflectionHints(RuntimeHints hints) {
+        // Register AbstractWebMvcEndpointHandlerMapping$OperationHandler for reflection
+        hints.reflection().registerType(
+                TypeReference.of("org.springframework.boot.webmvc.actuate.endpoint.web.AbstractWebMvcEndpointHandlerMapping$OperationHandler"),
+                typeHint -> typeHint.withMembers(MemberCategory.DECLARED_FIELDS)
+        );
+
+        // Register ServletWebOperation for reflection (related class that might be needed)
+        hints.reflection().registerType(
+                TypeReference.of("org.springframework.boot.webmvc.actuate.endpoint.web.AbstractWebMvcEndpointHandlerMapping$ServletWebOperation"),
+                typeHint -> typeHint.withMembers(MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_DECLARED_METHODS)
+        );
     }
 }
